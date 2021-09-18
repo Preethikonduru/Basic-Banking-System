5f0d33ccdcf256a1949ce4de6905cf9657571439
Preethi Konduru <71270683+Preethikonduru@users.noreply.github.com>
2021-09-18 17:45:32 +0530

    Add files via upload

---
+++ BasicBankingSystem/index.php
@@ -0,0 +1,36 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>Banking System</title>
+  </head>
+
+  <body style="background-color: #000000;">
+  <header>
+  <?php
+  include 'navbar.php';
+  ?></header>
+<main>
+  <style>
+  body{
+    background-image: url('img/bank.jpg');
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    width: 100%;
+    background-position: 210px 70px;
+  }
+</style>
+<h1 style = "position:relative; left:80px; top:80px; color:#C39EA0;  font-family: Italic;">W</h1>
+<h1 style = "position:relative; left:90px; top:80px; color:#C39EA0;  font-family: Italic;">E</h1>
+<h1 style = "position:relative; left:90px; top:80px; color:#C39EA0;  font-family: Italic;">L</h1>
+<h1 style = "position:relative; left:85px; top:80px; color:#C39EA0;  font-family: Italic;">C</h1>
+<h1 style = "position:relative; left:85px; top:80px; color:#C39EA0;  font-family: Italic;">O</h1>
+<h1 style = "position:relative; left:80px; top:80px; color:#C39EA0;  font-family: Italic;">M</h1>
+<h1 style = "position:relative; left:88px; top:80px; color:#C39EA0;  font-family: Italic;">E</h1>
+    </main>
+  </body>
+</html>
